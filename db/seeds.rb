9641db22e60d98e9e9778ab5c0cc0ade247a769a
haleydek <hcd09@live.com>
2019-07-25 09:59:32 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tswift = Artist.create!(name: 'Taylor Swift')
+tswift.songs.create!(title: 'Blank Space')
+tswift.songs.create!(title: 'Dress')
+tswift.songs.create!(title: 'Shake It Off')
+tswift.songs.create!(title: 'Archer')
+tswift.songs.create!(title: 'End Game')
+
+avicii = Artist.create!(name: 'Avicii')
+avicii.songs.create!(title: 'Without You')
+avicii.songs.create!(title: 'Levels')
+avicii.songs.create!(title: 'Peace of Mind')
+avicii.songs.create!(title: 'Tough Love')
+avicii.songs.create!(title: "Ain't A Thing")
+avicii.songs.create!(title: "The Days")
+avicii.songs.create!(title: "The Nights")
+avicii.songs.create!(title: "Wake Me Up")
